14c96ebc207e0b9e0c377bae14950f6b4853c2dd
zalim <zalimpshigotizhev@yandex.ru>
2025-01-24 20:30:45 +0300

    Обновление 0.0.1

--- apps/bot/app/const.py
+++ apps/bot/app/const.py
@@ -1,6 +1,5 @@
 import os
 
-
 DB_NAME = os.getenv('DB_NAME')
 DB_PORT = os.getenv('DB_PORT')
 DB_USER = os.getenv('DB_USER')
--- apps/bot/app/products/dao.py
+++ apps/bot/app/products/dao.py
@@ -5,8 +5,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 
 from const import asyncpg_url
 
-# engine = create_async_engine(url=asyncpg_url)
-engine = create_async_engine(url=f"postgresql+asyncpg://zalim:552216742@postgres:5432/db_parse")
+engine = create_async_engine(url=asyncpg_url)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
